b500af15a47b0a5fcaa7ac58d4e8c17374d56e5a
FlavienComet <fcomet.ing2022@esaip.org>
2021-06-22 22:34:26 +0200

    Début projet Login et page admin

---
+++ projet_majeur/src/Controller/AdminController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AdminController extends AbstractController
+{
+    /**
+     * @Route("/admin", name="admin")
+     * @IsGranted("ROLE_USER")
+     */
+    public function index(): Response
+    {
+        return $this->render('admin/index.html.twig', [
+            'controller_name' => 'AdminController',
+        ]);
+    }
+}
---
+++ projet_majeur/src/Controller/SecurityController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class SecurityController extends AbstractController
+{
+    /**
+     * @Route("/login", name="app_login")
+     */
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        // if ($this->getUser()) {
+        //     return $this->redirectToRoute('target_path');
+        // }
+
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+    }
+
+    /**
+     * @Route("/logout", name="app_logout")
+     */
+    public function logout()
+    {
+        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+
+//    /**
+//     * @Route("/addUser", name="addUser")
+//     */
+//    public function addUser(UserPasswordHasherInterface $encoder) {
+//
+//        $this->createUserAction($encoder, 'admin', 'y2fb95FQV');
+//        return new Response('Sucessful');
+//
+//    }
+//
+//    public function createUserAction($encoder, $username,$password) {
+//
+//
+//        $user = new User();
+//
+//        $user->setUsername($username);
+//        $user->setPassword($encoder->hashPassword($user, $password));
+//
+//        $em = $this->getDoctrine()->getManager();
+//        $em->persist($user);
+//        $em->flush();
+//
+//
+//    }
+}
